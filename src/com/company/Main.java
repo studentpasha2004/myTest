fc2015f0ea9259bb13e03077d3e79cdfa3a90559
Pavel Tkachenko <studentpasha2004@mail.ru>
2017-01-25 23:25:34 +0200

    It's a comment of my 2nd commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("My first line of code!!");
+        System.out.println("My first line of code!");
+        System.out.println("My second line of code!!");
     }
 }
